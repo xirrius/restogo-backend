e5ddbb60f14fe33bc63d79faed76726647a58acb
Shreya Nigam <shreya.nigam1062@gmail.com>
2024-06-17 03:10:20 +0530

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -10,9 +10,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'static'))
